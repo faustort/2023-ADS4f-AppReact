4fe0ed81a22e49948b3d52ea822c99fa42513f05
Your Name <you@example.com>
2023-10-11 20:27:07 -0300

    aula 11-10-23

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Image, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
+
+
+//<Image src={require('../assets/images/logo.jpg')} width={200}/>
 
 export default function HeaderBootstrap() {
     return (
         <Navbar>
             <Container>
-                <Navbar.Brand><Link to="/" className="nav-link" >App Juareis</Link></Navbar.Brand>
+                <Navbar.Brand>
+                    <Link to="/" className="nav-link" >
+                        <Image
+                            src={require("../assets/images/logosenac.png")}
+                            width={150}
+                        />
+                    </Link>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navegacao" />
-                <Navbar.Collapse id="navegacao">
+                <Navbar.Collapse id="navegacao" className="justify-content-end">
                     <Nav className="navbar-expand-lg navbar-light">
                         <Link to="/" className="nav-link">Inicial</Link>
                         <Link to="/sobre" className="nav-link">Sobre</Link>
