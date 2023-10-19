930eee2ec692d409b640fd12fdae511ff6ccfed3
Your Name <you@example.com>
2023-10-18 21:53:16 -0300

    Dicas quentes para o exercicio

--- src/screens/ContatoScreen.jsx
+++ src/screens/ContatoScreen.jsx
@@ -1,7 +1,56 @@
+import { Col, Container, Form, Row } from "react-bootstrap";
+
 export default function ContatoScreen() {
-    return (
-        <div>
-            <h1>Contato</h1>
-        </div>
-    )
+    /*
+    
+    Realizar a implementação do formulário de contato
+    com variáveis e anon functions para pegar os dados
+    e enviar para o backend usando o fetch
+    
+    */
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        fetch("", {
+            method: "POST",
+            body: JSON.stringify({
+                nome: 
+                email: ,
+            })
+        })
+    });
+    console.log(event);
+}
+
+return (
+    <Container>
+        <Row>
+            <Col lg={6} xs={12}>
+                <h1>Entre em </h1>
+                <h2>contato conosco</h2>
+                <p></p>
+            </Col>
+            <Col lg={6} xs={12}>
+                <Form>
+                    <Row>
+                        <Form.Group className="mb-3" as={Col} lg={6}>
+                            <Form.Label>Digite seu nome</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Digite o nome completo"
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col} lg={6}>
+                            <Form.Label>Digite seu E-mail</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Digite E-mail"
+                            />
+                        </Form.Group>
+                    </Row>
+                </Form>
+            </Col>
+        </Row>
+    </Container>
+)
 }
--- src/screens/NoticiasSingleScreen.jsx
+++ src/screens/NoticiasSingleScreen.jsx
@@ -11,7 +11,7 @@ export default function NoticiasSingleScreen() {
                 setNoticia(respostaEmJson);
             })
 
-    }, [])
+    }, [idNoticia])
     return (
         <Container>
             {noticia?.title &&
