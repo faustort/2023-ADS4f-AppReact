090da298a324b68992c0887e8f7e5462d92e2b24
Your Name <you@example.com>
2023-10-20 19:37:25 -0300

    fix

--- src/screens/ContatoScreen.jsx
+++ src/screens/ContatoScreen.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Col, Container, Form, FormGroup, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, FormGroup, Row } from "react-bootstrap";
 
 export default function ContatoScreen() {
     const [nome, setNome] = useState();
@@ -24,57 +24,64 @@ export default function ContatoScreen() {
         });
         console.log(event);
     }
-}
 
-return (
-    <Container>
-        <Row>
-            <Col lg={6} xs={12}>
-                <h1>Entre em </h1>
-                <h2>contato conosco</h2>
-                <p></p>
-            </Col>
-            <Col lg={6} xs={12}>
-                <Form>
-                    <Row>
-                        <Form.Group className="mb-3" as={Col} lg={6}>
-                            <Form.Label>Digite seu nome</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Digite o nome completo"
-                                value={nome}
-                                onChange={(event) => setNome(event.target.value)}
-                            />
-                        </Form.Group>
-                        <Form.Group as={Col} lg={6}>
-                            <Form.Label>Digite seu E-mail</Form.Label>
-                            <Form.Control
-                                type="email"
-                                placeholder="Digite E-mail"
-                            />
-                        </Form.Group>
-                        <FormGroup as={Col} >
-                            <Form.Label>Digite seu Telefone</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Digite seu Telefone"
-                                value={telefone}
-                                onChange={(event) => setTelefone(event.target.value)}
-                            />
-                        </FormGroup>
-                        <FormGroup as={Col} >
-                            <Form.Label>Digite sua Mensagem</Form.Label>
-                            <Form.Control
-                                as={'textarea'}
-                                placeholder="Digite sua Mensagem"
-                                value={mensagem}
-                                onChange={(event) => setMensagem(event.target.value)}
-                            />
-                        </FormGroup>
-                    </Row>
-                </Form>
-            </Col>
-        </Row>
-    </Container>
-)
+
+    return (
+        <Container>
+            <Row>
+                <Col lg={6} xs={12}>
+                    <h1>Entre em </h1>
+                    <h2>contato conosco</h2>
+                    <p></p>
+                </Col>
+                <Col lg={6} xs={12}>
+                    <Form>
+                        <Row>
+                            <Form.Group className="mb-3" as={Col} lg={6}>
+                                <Form.Label>Digite seu nome</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Digite o nome completo"
+                                    value={nome}
+                                    onChange={(event) => setNome(event.target.value)}
+                                />
+                            </Form.Group>
+                            <Form.Group as={Col} lg={6}>
+                                <Form.Label>Digite seu E-mail</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Digite E-mail"
+                                />
+                            </Form.Group>
+                            <FormGroup as={Col} >
+                                <Form.Label>Digite seu Telefone</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Digite seu Telefone"
+                                    value={telefone}
+                                    onChange={(event) => setTelefone(event.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup as={Col} >
+                                <Form.Label>Digite sua Mensagem</Form.Label>
+                                <Form.Control
+                                    as={'textarea'}
+                                    placeholder="Digite sua Mensagem"
+                                    value={mensagem}
+                                    onChange={(event) => setMensagem(event.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup as={Col}>
+                                <Button
+                                    variant="primary"
+                                    type="submit"
+                                    onClick={handleSubmit}
+                                />
+                            </FormGroup>
+                        </Row>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
