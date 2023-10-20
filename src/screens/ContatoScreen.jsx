19750a4312a0e7d049d74baa220fb757d8e32a8a
Your Name <you@example.com>
2023-10-20 19:34:27 -0300

    Contato aula 20-1

--- src/screens/ContatoScreen.jsx
+++ src/screens/ContatoScreen.jsx
@@ -1,14 +1,18 @@
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Col, Container, Form, FormGroup, Row } from "react-bootstrap";
 
 export default function ContatoScreen() {
+    const [nome, setNome] = useState();
+    const [email, setEmail] = useState();
+    const [telefone, setTelefone] = useState();
+    const [mensagem, setMensagem] = useState();
+    const [error, setError] = useState();
+
     /*
-    
     Realizar a implementação do formulário de contato
     com variáveis e anon functions para pegar os dados
     e enviar para o backend usando o fetch
-    
     */
-
     function handleSubmit(event) {
         event.preventDefault();
         fetch("", {
@@ -38,6 +42,8 @@ return (
                             <Form.Control
                                 type="text"
                                 placeholder="Digite o nome completo"
+                                value={nome}
+                                onChange={(event) => setNome(event.target.value)}
                             />
                         </Form.Group>
                         <Form.Group as={Col} lg={6}>
@@ -47,6 +53,24 @@ return (
                                 placeholder="Digite E-mail"
                             />
                         </Form.Group>
+                        <FormGroup as={Col} >
+                            <Form.Label>Digite seu Telefone</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Digite seu Telefone"
+                                value={telefone}
+                                onChange={(event) => setTelefone(event.target.value)}
+                            />
+                        </FormGroup>
+                        <FormGroup as={Col} >
+                            <Form.Label>Digite sua Mensagem</Form.Label>
+                            <Form.Control
+                                as={'textarea'}
+                                placeholder="Digite sua Mensagem"
+                                value={mensagem}
+                                onChange={(event) => setMensagem(event.target.value)}
+                            />
+                        </FormGroup>
                     </Row>
                 </Form>
             </Col>
