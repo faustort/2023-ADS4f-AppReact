e36e406b3f79bbda7b0d7e393adf3b8d4560acbf
Your Name <you@example.com>
2023-10-20 20:34:00 -0300

    fix cors

--- src/screens/ContatoScreen.jsx
+++ src/screens/ContatoScreen.jsx
@@ -2,86 +2,83 @@ import { useState } from "react";
 import { Button, Col, Container, Form, FormGroup, Row } from "react-bootstrap";
 
 export default function ContatoScreen() {
-    const [nome, setNome] = useState();
-    const [email, setEmail] = useState();
-    const [telefone, setTelefone] = useState();
-    const [mensagem, setMensagem] = useState();
-    const [error, setError] = useState();
+  const [nome, setNome] = useState();
+  const [email, setEmail] = useState();
+  const [telefone, setTelefone] = useState();
+  const [mensagem, setMensagem] = useState();
+  const [error, setError] = useState();
 
-    /*
-    Realizar a implementação do formulário de contato
-    com variáveis e anon functions para pegar os dados
-    e enviar para o backend usando o fetch
-    */
-    function handleSubmit(event) {
-        event.preventDefault();
-        fetch("", {
-            method: "POST",
-            body: JSON.stringify({
-                nome: aaa,
-                email: bbb,
-            })
-        });
-        console.log(event);
-    }
+  /*
+      Realizar a implementação do formulário de contato
+      com variáveis e anon functions para pegar os dados
+      e enviar para o backend usando o fetch
+      */
+  function handleSubmit(event) {
+    event.preventDefault(); // ???
 
+    fetch("http://localhost/rest/", {
+      method: "POST",
+      body: JSON.stringify({
+        nome: nome,
+        email: email,
+        telefone: telefone,
+        mensagem: mensagem,
+      }),
+    });
+    console.log(event);
+  }
 
-    return (
-        <Container>
+  return (
+    <Container>
+      <Row>
+        <Col lg={6} xs={12}>
+          <h1>Entre em </h1>
+          <h2>contato conosco</h2>
+          <p></p>
+        </Col>
+        <Col lg={6} xs={12}>
+          <Form>
             <Row>
-                <Col lg={6} xs={12}>
-                    <h1>Entre em </h1>
-                    <h2>contato conosco</h2>
-                    <p></p>
-                </Col>
-                <Col lg={6} xs={12}>
-                    <Form>
-                        <Row>
-                            <Form.Group className="mb-3" as={Col} lg={6}>
-                                <Form.Label>Digite seu nome</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Digite o nome completo"
-                                    value={nome}
-                                    onChange={(event) => setNome(event.target.value)}
-                                />
-                            </Form.Group>
-                            <Form.Group as={Col} lg={6}>
-                                <Form.Label>Digite seu E-mail</Form.Label>
-                                <Form.Control
-                                    type="email"
-                                    placeholder="Digite E-mail"
-                                />
-                            </Form.Group>
-                            <FormGroup as={Col} >
-                                <Form.Label>Digite seu Telefone</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Digite seu Telefone"
-                                    value={telefone}
-                                    onChange={(event) => setTelefone(event.target.value)}
-                                />
-                            </FormGroup>
-                            <FormGroup as={Col} >
-                                <Form.Label>Digite sua Mensagem</Form.Label>
-                                <Form.Control
-                                    as={'textarea'}
-                                    placeholder="Digite sua Mensagem"
-                                    value={mensagem}
-                                    onChange={(event) => setMensagem(event.target.value)}
-                                />
-                            </FormGroup>
-                            <FormGroup as={Col}>
-                                <Button
-                                    variant="primary"
-                                    type="submit"
-                                    onClick={handleSubmit}
-                                />
-                            </FormGroup>
-                        </Row>
-                    </Form>
-                </Col>
+              <Form.Group className="mb-3" as={Col} lg={6}>
+                <Form.Label>Digite seu nome</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Digite o nome completo"
+                  value={nome}
+                  onChange={(event) => setNome(event.target.value)}
+                />
+              </Form.Group>
+              <Form.Group as={Col} lg={6}>
+                <Form.Label>Digite seu E-mail</Form.Label>
+                <Form.Control type="email" placeholder="Digite E-mail" />
+              </Form.Group>
+              <FormGroup as={Col}>
+                <Form.Label>Digite seu Telefone</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Digite seu Telefone"
+                  value={telefone}
+                  onChange={(event) => setTelefone(event.target.value)}
+                />
+              </FormGroup>
+              <FormGroup as={Col}>
+                <Form.Label>Digite sua Mensagem</Form.Label>
+                <Form.Control
+                  as={"textarea"}
+                  placeholder="Digite sua Mensagem"
+                  value={mensagem}
+                  onChange={(event) => setMensagem(event.target.value)}
+                />
+              </FormGroup>
+              <FormGroup as={Col} lg={12}>
+                <Button variant="primary" type="submit" onClick={handleSubmit}>
+                  Enviar contato
+                </Button>
+              </FormGroup>
             </Row>
-        </Container>
-    )
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
