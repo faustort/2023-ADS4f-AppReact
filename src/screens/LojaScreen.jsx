b09bcdcf5dbfa23cc7deed44478f11e0e59a8bbe
Your Name <you@example.com>
2023-10-20 21:04:25 -0300

    usar o api

--- src/screens/LojaScreen.jsx
+++ src/screens/LojaScreen.jsx
@@ -5,6 +5,9 @@ import { Container } from "react-bootstrap";
  * Façam a tela de loja aqui
  */
 export default function LojaScreen() {
+  
+    // https://fakestoreapi.com/products/
+
   return (
     <Container>
       <h1>Loja</h1>
