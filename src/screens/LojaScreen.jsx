75d9032a8c7e1856f0f7468736639643a3bb3a8b
Your Name <you@example.com>
2023-10-20 21:07:59 -0300

    ex

--- src/screens/LojaScreen.jsx
+++ src/screens/LojaScreen.jsx
@@ -6,24 +6,27 @@ import { Link } from "react-router-dom";
  * Façam a tela de loja aqui
  */
 export default function LojaScreen() {
-  // https://fakestoreapi.com/products/
+  
+    // https://fakestoreapi.com/products/
+  
 
   return (
     <Container>
       <h1>Loja</h1>
       <Container>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+        {/* Aqui será um loop */}
+        <Card>
+          <Card.Img variant="top" src="{imagem}" />
           <Card.Body>
-            <Card.Title>Nome Do Produto</Card.Title>
-            <Card.Text>Descrição do Protudo</Card.Text>
+            <Card.Title>{Nome Do Produto}</Card.Title>
+            <Card.Text>{Descrição do Produto}</Card.Text>
           </Card.Body>
           <ListGroup className="list-group-flush">
-            <ListGroup.Item>Price</ListGroup.Item>
-            <ListGroup.Item>Category</ListGroup.Item>
+            <ListGroup.Item>{Price}</ListGroup.Item>
+            <ListGroup.Item>{Category}</ListGroup.Item>
           </ListGroup>
           <Card.Body>
-            <Card.Link href="#" as={Link}>
+            <Card.Link to="/???" as={Link}>
               Ver Produto
             </Card.Link>
           </Card.Body>
